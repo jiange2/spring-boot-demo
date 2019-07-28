f0cc4b6fa3d6f93e3f2ca3a92b690e9e2a2275c7
jiange2 <992975556@qq.com>
2019-07-29 02:24:08 +0800

    update

--- spring-boot-base/src/test/java/com/jiange2/springbootbase/config/Configuration1.java
+++ spring-boot-base/src/test/java/com/jiange2/springbootbase/config/Configuration1.java
@@ -1,18 +1,47 @@
 package com.jiange2.springbootbase.config;
 
-import com.jiange2.springbootbase.config.service.Service1;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.boot.web.servlet.context.AnnotationConfigServletWebServerApplicationContext;
+import org.springframework.boot.web.servlet.context.ServletWebServerApplicationContext;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 @Configuration
 @Import(Configuration2.class)
 public class Configuration1 {
 
     public static void main(String[] args){
-        ApplicationContext context =
-                new AnnotationConfigApplicationContext(Configuration1.class);
-        System.out.println(context.getBean(Service1.class));
+        AnnotationConfigServletWebServerApplicationContext context =
+                new AnnotationConfigServletWebServerApplicationContext(Configuration1.class);
+    }
+
+    @Bean
+    public TomcatServletWebServerFactory tomcatServletWebServerFactory() {
+        return new TomcatServletWebServerFactory();
+    }
+
+    @Bean
+    public ServletRegistrationBean<TestServlet> servletRegistration(){
+        ServletRegistrationBean<TestServlet> servletRegistrationBean = new ServletRegistrationBean<>();
+        servletRegistrationBean.addUrlMappings("/hello");
+        servletRegistrationBean.setServlet(new TestServlet());
+        return servletRegistrationBean;
+    }
+
+    static class TestServlet extends HttpServlet {
+        @Override
+        protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+            resp.getWriter().write("Hello World");
+        }
     }
 }
